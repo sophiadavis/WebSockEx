0047acce6d2078da549555776322a1be214b1119
Sophia Davis <scdgrapefruit@gmail.com>
2014-09-09 20:29:49 -0400

    Refactor server code

--- lib/web_sock_ex/server.ex
+++ lib/web_sock_ex/server.ex
@@ -2,7 +2,7 @@ defmodule WebSockEx.Server do
 	require Logger
 
 	@ws_guid "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-	@sec_key_pattern ~r/Sec-WebSocket-Key: (?<key>.*)\r\n/r
+	@sec_key_pattern ~r/Sec-WebSocket-Key: (?<key>.*)($|\r\n)/r
 
 	def accept port do
 		{:ok, socket} = :gen_tcp.listen(port,
@@ -20,23 +20,22 @@ defmodule WebSockEx.Server do
 	defp serve socket do
 		{:ok, data} = :gen_tcp.recv(socket, 0) # 0 -- return all available bytes
 		Logger.debug "Client connected."
-		response = String.strip data |> verify_ws_connection
-		Logger.debug "\nResponse: \n" <> IO.inspect response
-		:gen_tcp.send(socket, response <> "\r\n\r\n")
-		Logger.debug "Response sent"
+		String.strip(data) |>
+			handle_ws_connection |>
+			write socket
 	end
 
-	def parse_handshake request do
-		[sec_key] = Regex.run @sec_key_pattern, request, [capture: :all_names]
+	defp parse_and_make_response handshake do
+		[sec_key] = Regex.run @sec_key_pattern, handshake, [capture: :all_names]
 		data = sec_key <> @ws_guid
 		:crypto.hash(:sha, data) |>
 			:base64.encode |>
 			make_response_handshake
 	end
 
-	defp verify_ws_connection request do # TODO return tuples, so client socket can be closed if needed
+	defp handle_ws_connection request do # TODO return tuples, so client socket can be closed if needed
 		cond do
-			String.contains? request, "Upgrade:" -> parse_handshake request
+			String.contains? request, "Upgrade:" -> parse_and_make_response request
 			true ->
 				Logger.debug "Non-ws request received."
 				"HTTP/1.1 501 Not implemented\r\n\r\n"
@@ -45,5 +44,10 @@ defmodule WebSockEx.Server do
 
 	defp make_response_handshake response_key do
 		"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: #{response_key}\r\n\r\n"
+	end
+
+	defp write response, socket do
+		Logger.debug "\nResponse: \n" <> response
+		:gen_tcp.send(socket, response)
 	end
 end
