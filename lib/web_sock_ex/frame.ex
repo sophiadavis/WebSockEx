65766b869c6d46f7c5eff582001d41cd7c2f5a3b
Sophia Davis <scdgrapefruit@gmail.com>
2014-09-11 15:19:07 -0400

    Refactor frame functions

--- lib/web_sock_ex/frame.ex
+++ lib/web_sock_ex/frame.ex
@@ -22,11 +22,20 @@ defmodule WebSockEx.Frame do
 	|                     Payload Data continued ...                |
 	+---------------------------------------------------------------+
 '''
-	@no_mask 0
-	@mask 1
+	@no_mask <<0::size(1)>>
+	@mask 	 <<1::size(1)>>
 
-	@not_last_frame 0
-	@last_frame 1
+	## fin bit
+	@final 		<<1::size(1)>>
+	@fragment <<0::size(1)>>
+
+	## opcode definitions
+	@continuation <<0::size(1), 0::size(1), 0::size(1), 0::size(1)>> # 0
+	@text					<<0::size(1), 0::size(1), 0::size(1), 1::size(1)>> # 1
+	@binary				<<0::size(1), 0::size(1), 1::size(1), 0::size(1)>> # 2
+	@close				<<1::size(1), 0::size(1), 0::size(1), 0::size(1)>> # 8
+	@ping 				<<1::size(1), 0::size(1), 0::size(1), 1::size(1)>> # 9
+	@pong					<<1::size(1), 0::size(1), 1::size(1), 0::size(1)>> # A
 
 	def parse_frame <<fin::bits-size(1), rsvs::bits-size(3), rest::bits>> do
 		# TODO -- assert that
@@ -34,31 +43,21 @@ defmodule WebSockEx.Frame do
 
 		case fin do
 			<<1::size(1)>> ->
-				{:ok, parse_final_frame(rest, [])}
+				parse_final_frame rest
 			<<0::size(1)>> ->
-				{:ok, parse_incomplete_frame(rest, [])}
+				parse_incomplete_frame rest
 		end
 	end
 
-	defp parse_final_frame msg, current_payload do
+	defp parse_incomplete_frame msg do
+		:unimplemented
+	end
+
+	defp parse_final_frame rest do
 		<<opcode::bits-size(4),
 			mask::bits-size(1),
 			payload_len::bits-size(7),
-			rest::bits>> = msg
-
-		# TODO -- assert that
-		<<1::size(1)>> = mask
-
-		Logger.debug "Opcode: #{inspect opcode}"
-		# TODO deal w opcode, and it's hex??
-			# *  %x0 denotes a continuation frame
-			# *  %x1 denotes a text frame
-			# *  %x2 denotes a binary frame
-			# *  %x3-7 are reserved for further non-control frames
-			# *  %x8 denotes a connection close
-			# *  %x9 denotes a ping
-			# *  %xA denotes a pong
-			# *  %xB-F are reserved for further control frames
+			rest::bits>> = rest
 
 		cond do
 			payload_len == 126 ->
@@ -67,12 +66,18 @@ defmodule WebSockEx.Frame do
 				<<payload_len::bits-size(64), payload::bits>> = rest
 			true -> payload = rest
 		end
-		translate_payload payload
+		{:ok, :final, parse_mask(mask), parse_opcode(opcode), translate_payload(payload)}
 	end
 
-	defp parse_incomplete_frame msg, current_payload do
-		:unimplemented
-	end
+	defp parse_mask(@no_mask), do: :unmasked
+	defp parse_mask(@mask), do: :masked
+
+	defp parse_opcode(@continuation), do: :cont
+	defp parse_opcode(@text), do: :text
+	defp parse_opcode(@binary), do: :binary
+	defp parse_opcode(@close), do: :close
+	defp parse_opcode(@ping), do: :ping
+	defp parse_opcode(@pong), do: :pong
 
 	def translate_payload <<masking_key::bits-size(32), payload::bits>> do
 	'''
@@ -93,13 +98,24 @@ defmodule WebSockEx.Frame do
 					i + 1,
 					decoded <> <<bxor(m,n)>>
 	end
+
+	def format_server_frame payload, :text do
+		# TODO accurate payload lengths...
+		<<@final::bits, 0::size(3), @text::bits,
+			@no_mask::bits, byte_size(payload)::size(7), payload::binary>>
+	end
+
+	def format_server_frame payload, :close do
+		# TODO accurate payload lengths...
+		<<@final::bits, 0::size(3), @close::bits,
+			@no_mask::bits, byte_size(payload)::size(7), payload::binary>>
 	end
 
 	def format_client_frame masking_key, payload, opcode do
 		# TODO accurate payload lengths...
 		IO.inspect payload
 		IO.inspect masking_key
-		<<@last_frame::size(1), 0::size(3), opcode::size(4), @mask::size(1),
+		<<@final::bits, 0::size(3), opcode::bits, @mask::bits,
 			byte_size(payload)::size(7), masking_key::binary, payload::binary>>
 	end
 end
